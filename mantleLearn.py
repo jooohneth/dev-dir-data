6db8651b50272c20015eca7b83ebc59b888a6b2d
Thai Nguyen <jooohn.eth@gmail.com>
2024-06-11 21:34:23 +0200

    add getBadEmails

--- mantleLearn.py
+++ mantleLearn.py
@@ -2,3 +2,10 @@ import pandas as pd
 
 data = pd.read_csv("input.csv")
 emails = data["teamEmails"]
+
+def getBadEmails(data, emails):
+    temp = data[emails.str.contains("@") == False]
+    temp.to_csv("badEmails.csv")
+    return temp.index
+
+print(getBadEmails(data, emails))
