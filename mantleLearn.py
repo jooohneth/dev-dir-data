0805f19903d25d7682389e8817f2d0c956453f6c
Thai Nguyen <jooohn.eth@gmail.com>
2024-06-15 20:05:04 -0400

    added implementation

--- mantleLearn.py
+++ mantleLearn.py
@@ -21,3 +21,17 @@ def getRecordsWtihMultipleEmails(data, emails):
 def swapColumns(data, indexes, colOne, colTwo):
     data.loc[indexes, [colOne, colTwo]] = data.loc[indexes, [colTwo, colOne]].values
 
+mantleLearnIndexes = getMantleLearnRecords(data, emails)
+swapColumns(data, mantleLearnIndexes, 'Project Name', 'teamEmails')
+
+badIndexes = getBadEmails(data, emails)
+cleanedData = data.drop(badIndexes)
+
+multipleEmailsIndexes = getRecordsWtihMultipleEmails(cleanedData, emails)
+cleanedData.loc[multipleEmailsIndexes, 'teamEmails'] = cleanedData.loc[multipleEmailsIndexes, 'teamEmails'].str.split(',')
+
+explodedData = cleanedData.explode('teamEmails')
+
+uniqueData = explodedData.drop_duplicates(subset='teamEmails', keep='first')
+
+uniqueData['teamEmails'].to_csv("output/cleaned_emails.csv", index=False, header=True)
