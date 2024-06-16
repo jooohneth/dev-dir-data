00f1bdff75b45ffe715249c3e1df30159d23244b
Thai Nguyen <jooohn.eth@gmail.com>
2024-06-15 20:03:36 -0400

    updates getBadEmails

--- mantleLearn.py
+++ mantleLearn.py
@@ -4,8 +4,8 @@ data = pd.read_csv("input.csv")
 emails = data["teamEmails"]
 
 def getBadEmails(data, emails):
-    temp = data[emails.str.contains("@") == False]
-    temp.to_csv("badEmails.csv")
+    temp = data[emails.str.contains("@", na=False) == False]
+    # temp.to_csv("output/badEmails.csv")
     return temp.index
 
-print(getBadEmails(data, emails))
+
