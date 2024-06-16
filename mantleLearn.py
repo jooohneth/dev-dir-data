d7fed64783efd2c4177aabff10dace8c6dca6cd9
Thai Nguyen <jooohn.eth@gmail.com>
2024-06-15 20:04:01 -0400

    getting mantle learn records

--- mantleLearn.py
+++ mantleLearn.py
@@ -8,4 +8,9 @@ def getBadEmails(data, emails):
     # temp.to_csv("output/badEmails.csv")
     return temp.index
 
+def getMantleLearnRecords(data, emails):
+    temp = data[emails.str.contains("Mantle") == True]
+    # temp.to_csv("output/mantleLearnRecords.csv")
+    return temp.index
+
 
